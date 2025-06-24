14ef92e63d5db4c0557c29f80b0358ee5284def2
Muhammad Hamza <hxadogar@gmail.com>
2025-06-24 09:11:33 +0500

    solved [singly linked list]

---
+++ problems/0876-middle-of-the-linked-list.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//  * Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* cur = head;
+        int counter = 0;
+        
+        while(head->next != nullptr) {
+            counter++;
+            head = head->next;
+        }
+        cout << counter;
+        
+        if(counter % 2 == 0) {
+            counter = counter / 2;
+        } else {
+            counter = counter / 2 + 1;
+        }
+
+        for(int i = 0; i < counter; i++) {
+           cur = cur->next; 
+        }
+        return cur;
+    }
+};
